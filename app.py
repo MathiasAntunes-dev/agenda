59ee263c229ec53c650288d46250623ede38bd02
MathiasAntunes-dev <mathiasant1407@gmail.com>
2026-02-09 16:42:19 -0300

    estilizando a pagina agenda

--- app.py
+++ app.py
@@ -24,8 +24,21 @@ def agenda():
 def delete(idTarefa):
     tarefa = Tarefa.id(idTarefa)
     tarefa.excluir_tarefa()
-    # return render_template('agenda.html', titulo="Agenda", tarefas=)
+    # return render_template('agenda.html', titulo="Agenda", tarefas="Agenda", tarefas=tarefas)
     return redirect(url_for('agenda'))
+
+@app.route('/update/<int:idTarefa>', methods=['GET', 'POST'])
+def update(idTarefa):
+    if request.method == 'POST':
+        titulo = request.form['titulo-tarefa']
+        data = request.form['data-conclusao']
+        tarefa = Tarefa(titulo, data, idTarefa)
+        tarefa.atualizar_tarefa()
+        return redirect(url_for('agenda')) # early return
+
+    tarefas = Tarefa.obter_tarefas()
+    tarefa_selecionada = Tarefa.id(idTarefa) # seleção da tarefa que será editada
+    return render_template('agenda.html', titulo=f'Editando a tarefa ID: {idTarefa}', tarefas=tarefas, tarefa_selecionada=tarefa_selecionada)
 
 @app.route('/ola')
 def ola_mundo():
