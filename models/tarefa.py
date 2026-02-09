a17bf1bb8d58016f941b43187f7b04f8858f9f10
MathiasAntunes-dev <mathiasant1407@gmail.com>
2026-02-09 10:36:25 -0300

    refact: declarando explicitamente os tipos
    As classes Tarefa e Database tiveram os tipos declarados explicitamente, incluindo tratamento de excessões no método especial `__exit__`.

--- models/tarefa.py
+++ models/tarefa.py
@@ -1,31 +1,32 @@
-from typing import Any, Self
 from models.database import Database
+from typing import Self, Any, Optional
 from sqlite3 import Cursor
 
-
 class Tarefa:
-    def __init__(self: Self, titulo_tarefa: str, data_conclusao: str = None, id: int = None) -> None:
-        self.titulo_tarefa: str = titulo_tarefa
-        self.data_conclusao: str = data_conclusao
-        self.id_tarefa: int = id
-
+    def __init__(self: Self, titulo_tarefa: Optional[str] = '', data_conclusao: Optional[str] = None, id_tarefa: Optional[int] = None) -> None:
+        self.titulo_tarefa: Optional[str] = titulo_tarefa
+        self.data_conclusao: Optional[str] = data_conclusao
+        self.id_tarefa: Optional[int] = id_tarefa
+    
     # Sem o conceito de sobrecarga
     # Tarefa(titulo_tarefa="Nova tarefa")
     # Tarefa(titulo_tarefa="Outra tarefa", data_conclusao="2026-02-03")
     # Tarefa(id_tarefa=1)
 
     @classmethod
-    def id(cls, id: int):
+    def id(cls, id: int) -> Self:
         with Database('./data/tarefas.sqlite3') as db:
             query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
             params: tuple = (id,)
-            resultado = db.buscar_tudo(query, params)
+            resultado: list[Any] = db.buscar_tudo(query, params)
+            # resultado = [["titulo_tarefa", "data_conclusao"]]
 
             # Desempacotamento de coleção
             [[titulo, data]] = resultado
-
+            
         return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
     
+    # Simulando o conceito de sobrecarga
     # Tarefa('Título da Tarefa')
     # Tarefa('Título da Tarefa', '2026-02-03')
     # Tarefa.id(1)
@@ -36,17 +37,18 @@ class Tarefa:
             params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
 
-    @staticmethod
-    def obter_tarefas() -> list[Self]:
+    @classmethod
+    def obter_tarefas(cls) -> list[Self]:
         with Database('./data/tarefas.sqlite3') as db:
             query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas;'
             resultados: list[Any] = db.buscar_tudo(query)
-            tarefas: list[Self] = [Tarefa(titulo, data) for titulo, data in resultados]
+            tarefas: list[Self] = [cls(titulo, data) for titulo, data in resultados]
             return tarefas
         
-    def excluir_tarefa(self):
+    def excluir_tarefa(self) -> Cursor:
         with Database('./data/tarefas.sqlite3') as db:
             query: str = 'DELETE FROM tarefas WHERE id = ?;'
             params: tuple = (self.id_tarefa,)
             resultado: Cursor = db.executar(query, params)
             return resultado
+    
